4b591117b6454dcad4cce7e322e206886693a6d1
mr3bzo <eabdelrahman18@gmail.com>
2024-12-12 19:01:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System;
+#region Q1
+class Q1
+{
+    static void Main()
+    {
+
+        int number = 5;
+        Console.WriteLine(number);
+    }
+}
+#endregion
+#region Q2
+
+class Q2
+{
+    static void Main()
+    {
+        int x = 10;
+        int y = x; 
+        y = 20;    
+
+        Console.WriteLine(x); // Output: 10
+        Console.WriteLine(y); // Output: 20
+    }
+}
+// When assigning one value type to another, a copy of the value is made. Changes to one variable do not affect the other
+#endregion
+#region Q3
+
+class Q3
+{
+    static void Main()
+    {
+        string str1 = "Hello";
+        string str2 = str1; 
+        str2 = "World";     
+
+        Console.WriteLine( str1);
+        Console.WriteLine( str2); 
+    }
+}
+
+#endregion
+
